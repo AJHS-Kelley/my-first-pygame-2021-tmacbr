33d3582653be1b027ed506ff4360f3b9b84bbb7f
Tristan mckay <90346589+tmacbr@users.noreply.github.com>
2021-12-01 14:32:51 -0500

    Update myFirstPyGame.py

--- myFirstPyGame.py
+++ myFirstPyGame.py
@@ -1,6 +1,7 @@
 # my first pygame, Tristan Mckay, 11/29/21 159 p.m, v0.0
 
 import pygame, sys
+from pygame.font import SysFont
 from pygame.locals import * 
 
  # start pygame 
@@ -18,13 +19,13 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 DEPRESSIONPURP = (193, 75, 253)
 # setup font
-basicfont = pygame.font.Sysfont(None, 48)
+basicfont = pygame.font.SysFont(None, 48)
 
 # setup text.
 text = basicfont.render('hello, world!', True, WHITE, BLUE)
 textrect = text.get_rect()
-textrect.centerx = WindowSurface.get_rect().certerx
-textrect.centery = WindowSurface.get_rect().certery
+textrect.centerx = WindowSurface.get_rect().centerx
+textrect.centery = WindowSurface.get_rect().centery
 
 # fill background color
 WindowSurface.fill(WHITE)
@@ -48,9 +49,19 @@ pygame.draw.ellipse(WindowSurface, BLUE, ( 300, 250, 40, 80), 1)
 pygame.draw.rect(WindowSurface, BLUE, (textrect.left - 20, textrect.top - 20, textrect.width + 40, textrect.height + 40))
 
 # create pixel array
-pixArray = pygame.pixelarray(WindowSurface)
+pixArray = pygame.PixelArray(WindowSurface)
 pixArray[480][380] = BLACK
 del pixArray
 
 # draw the text onto the surface.
 WindowSurface.blit(text, textrect)
+
+# update pygame display
+pygame.display.update()
+
+# run game into loop
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
