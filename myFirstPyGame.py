f3cc4a30068d59b00494735d9006e4e527683f5c
Tristan mckay <90346589+tmacbr@users.noreply.github.com>
2021-12-01 14:15:01 -0500

    v0.9

--- myFirstPyGame.py
+++ myFirstPyGame.py
@@ -36,3 +36,21 @@ pygame.draw.polygon(WindowSurface, DEPRESSIONPURP, ((146,0), (291, 106), (236,27
 pygame.draw.line(WindowSurface, RED, (60,60), (120, 60), 4)
 pygame.draw.line(WindowSurface, GREEN, (60,40), (120, 40), 3)
 pygame.draw.line(WindowSurface, BLACK, (50,60), (100, 60), 2)
+
+
+ # draw a circle
+pygame.draw.circle(WindowSurface, WHITE,(300, 50), 20, 0)
+
+# draw an ellispe
+pygame.draw.ellipse(WindowSurface, BLUE, ( 300, 250, 40, 80), 1)
+
+# draw the text rectangle
+pygame.draw.rect(WindowSurface, BLUE, (textrect.left - 20, textrect.top - 20, textrect.width + 40, textrect.height + 40))
+
+# create pixel array
+pixArray = pygame.pixelarray(WindowSurface)
+pixArray[480][380] = BLACK
+del pixArray
+
+# draw the text onto the surface.
+WindowSurface.blit(text, textrect)
